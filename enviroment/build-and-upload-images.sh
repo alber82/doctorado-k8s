4107cc94ea8304912344f3a5e29637f97bb9d7a2
Alberto Gomez <alber82@gmail.com>
2024-10-24 11:02:57 +0000

    .

--- enviroment/build-and-upload-images.sh
+++ enviroment/build-and-upload-images.sh
@@ -6,7 +6,7 @@ export DOCKER_BUILDKIT=1
 #docker build --platform linux/arm64 --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/doctorado/scheduler:0.0.0 -f ../scheduler/Dockerfile ..
 #docker push 192.168.1.229:5000/doctorado/scheduler:0.0.0
 
-docker build --platform linux/arm64 --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/doctorado/scheduler-operator:0.0.0 -f ../operator/src/main/go/Dockerfile ..
+docker build --platform linux/arm64 --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/doctorado/scheduler-operator:0.0.0 -f operator/Dockerfile .
 docker push 192.168.1.229:5000/doctorado/scheduler-operator:0.0.0
 
 #docker build --build-arg VERSION=0.0.0 -t localhost:5000/albertogomez/random-scheduler:0.0.0 -f ../../random-scheduler/Dockerfile ../..
--- operator/cmd/main.go
+++ operator/cmd/main.go
@@ -178,12 +178,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MetricScheduler")
 		os.Exit(1)
 	}
-	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err = (&batchv1.MetricScheduler{}).SetupWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "MetricScheduler")
-			os.Exit(1)
-		}
-	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
