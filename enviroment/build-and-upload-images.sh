174fefb2b9a52e2172bb6488384f0c78c483ff37
Alberto Gomez <alber82@gmail.com>
2024-12-01 18:00:53 +0100

    nginx tests

--- enviroment/build-and-upload-images.sh
+++ enviroment/build-and-upload-images.sh
@@ -3,7 +3,6 @@ set -e
 
 export DOCKER_BUILDKIT=1
 
-
 docker build --platform linux/arm64 --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/doctorado/scheduler-operator:0.0.0 -f operator/Dockerfile .
 docker push 192.168.1.229:5000/doctorado/scheduler-operator:0.0.0
 
@@ -12,3 +11,6 @@ docker push 192.168.1.229:5000/doctorado/ts-scheduler:0.0.0
 
 docker build --platform linux/arm64  --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/albertogomez/influxdb-scheduler:0.0.0 -f influxmetricsscheduler/Dockerfile .
 docker push 192.168.1.229:5000/doctorado/influxdb-scheduler:0.0.0
+
+docker build --platform linux/arm64  --build-arg VERSION=0.0.0 -t 192.168.1.229:5000/albertogomez/random-scheduler:0.0.0 -f random-scheduler/Dockerfile .
+docker push 192.168.1.229:5000/doctorado/random-scheduler:0.0.0
