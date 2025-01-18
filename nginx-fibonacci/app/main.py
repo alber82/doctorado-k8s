cc0af549e3e1f9840b5658fd2600547d19de3564
Alberto Gomez <alber82@gmail.com>
2025-01-18 16:44:05 +0100

    nginx tests

--- nginx-fibonacci/app/main.py
+++ nginx-fibonacci/app/main.py
@@ -12,7 +12,8 @@ def read_root():
 # Will result in http://127.0.0.1:8000/next-fibonacci?number=x
 @app.get("/next-fibonacci")
 def read_item(number: int):
-    return next_fibonacci(number)
+#     return next_fibonacci(number)
+     return fibonacci(number)
 
 def next_fibonacci(n):
     fib = [0, 1]
@@ -22,3 +23,17 @@ def next_fibonacci(n):
         return {"result" : fib[-1] + fib[-2]}
     else:
         return {"error" : "Not a fibonacci number"}
+
+def fibonacci(n):
+    n = int(n)
+
+    def fibon(a,b,n,result):
+        c = a+b
+        result.append(c)
+        if c < n:
+            fibon(b,c,n,result)
+        return result
+
+    return fibon(0,1,n,[])
+
+print(fibonacci(input("Input a number to print sequence up to: ")))
