1a9e6ccaf9be829ebe2c6e6dcd44a3d9559012bd
Alberto Gomez <alber82@gmail.com>
2025-01-18 23:13:10 +0100

    nginx tests

---
+++ tests/nginx/cpu/nginx-test.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+export
+const options = {
+    vus: 10,
+    duration: '600s',
+};
+
+export default function
+    () {
+    let i =randomIntBetween(1, 100)
+    const response = http.get('http://nginx-project.nginx/next-fibonacci?number=8' + i);
+
+    check(response,
+        {
+            'status was 200': r => r.status === 200,
+        });
+}
