84d00fe1959f6dd1742820aba1794553598864f9
Alberto Gomez <alber82@gmail.com>
2025-02-22 16:03:57 +0100

    nginx tests

--- enviroment/install.sh
+++ enviroment/install.sh
@@ -11,10 +11,10 @@ helm install kube-prometheus-stack   --create-namespace   --namespace monitoring
 helm install k6-operator grafana/k6-operator -f values.yaml
 
 # instalacion kafka
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kraft/kafka-single-node.yaml -n kafka
-kubectl -n kafka apply -f 'https://strimzi.io/install/latest?namespace=kafka'
-
-
-#test
-kubectl create configmap postgres-stress-test --from-file test/postgres/postgres-test.js
-kubectl create configmap kafka-stress-test --from-file test/kafka/kafka-test.js
+#kubectl apply -f https://strimzi.io/examples/latest/kafka/kraft/kafka-single-node.yaml -n kafka
+#kubectl -n kafka apply -f 'https://strimzi.io/install/latest?namespace=kafka'
+#
+#
+##test
+#kubectl create configmap postgres-stress-test --from-file test/postgres/postgres-test.js
+#kubectl create configmap kafka-stress-test --from-file test/kafka/kafka-test.js
