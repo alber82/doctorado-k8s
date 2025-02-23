f761a187809deb305b505204074e3863c20a9f6a
Alberto Gomez <alber82@gmail.com>
2025-02-23 11:38:57 +0100

    nginx tests

--- tests/nginx/nginx-test.js
+++ tests/nginx/nginx-test.js
@@ -3,26 +3,26 @@ import http from 'k6/http';
 
 export let options = {
     stages: [
-        { duration: '10m', target: 250 },   // 500 RPS en los primeros 10 minutos
-        { duration: '10m', target: 500 },   // Aumento del 5% a 525 RPS durante 10 minutos
-        { duration: '10m', target: 750 },   // Aumento del 5% a 551 RPS durante 10 minutos
-        { duration: '10m', target: 1000 },   // Aumento del 5% a 578 RPS durante 10 minutos
-        { duration: '10m', target: 1250 },   // Aumento del 5% a 607 RPS durante 10 minutos
-        { duration: '10m', target: 1500 },   // Aumento del 5% a 637 RPS durante 10 minutos
-        { duration: '10m', target: 1750 },   // Aumento del 5% a 669 RPS durante 10 minutos
-        { duration: '10m', target: 2000 },   // Aumento del 5% a 702 RPS durante 10 minutos
-        { duration: '10m', target: 2250 },   // Aumento del 5% a 737 RPS durante 10 minutos
-        { duration: '10m', target: 2500 },   // Aumento del 5% a 773 RPS durante 10 minutos
-        { duration: '10m', target: 2750 },   // Aumento del 5% a 811 RPS durante 10 minutos
-        { duration: '10m', target: 3000 },   // Aumento del 5% a 851 RPS durante 10 minutos
-        { duration: '10m', target: 3250 },   // Aumento del 5% a 893 RPS durante 10 minutos
-        { duration: '10m', target: 3500 },   // Aumento del 5% a 937 RPS durante 10 minutos
-        { duration: '10m', target: 3750 },   // Aumento del 5% a 984 RPS durante 10 minutos
-        { duration: '10m', target: 4000 }, // Aumento del 5% a 1,033 RPS durante 10 minutos
-        { duration: '10m', target: 4250 }, // Aumento del 5% a 1,085 RPS durante 10 minutos
-        { duration: '10m', target: 4500 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
-        { duration: '10m', target: 4750 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
-        { duration: '10m', target: 5000 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
+        { duration: '5m', target: 200 },   // 500 RPS en los primeros 10 minutos
+        { duration: '5m', target: 400 },   // Aumento del 5% a 525 RPS durante 10 minutos
+        { duration: '5m', target: 600 },   // Aumento del 5% a 551 RPS durante 10 minutos
+        { duration: '5m', target: 800 },   // Aumento del 5% a 578 RPS durante 10 minutos
+        { duration: '5m', target: 1000 },   // Aumento del 5% a 607 RPS durante 10 minutos
+        { duration: '5m', target: 1200 },   // Aumento del 5% a 637 RPS durante 10 minutos
+        { duration: '5m', target: 1400 },   // Aumento del 5% a 669 RPS durante 10 minutos
+        { duration: '5m', target: 1600 },   // Aumento del 5% a 702 RPS durante 10 minutos
+        { duration: '5m', target: 1800 },   // Aumento del 5% a 737 RPS durante 10 minutos
+        { duration: '5m', target: 2000 },   // Aumento del 5% a 773 RPS durante 10 minutos
+        { duration: '5m', target: 2200 },   // Aumento del 5% a 811 RPS durante 10 minutos
+        { duration: '5m', target: 2400 },   // Aumento del 5% a 851 RPS durante 10 minutos
+        { duration: '5m', target: 2600 },   // Aumento del 5% a 893 RPS durante 10 minutos
+        { duration: '5m', target: 2800 },   // Aumento del 5% a 937 RPS durante 10 minutos
+        { duration: '5m', target: 3000 },   // Aumento del 5% a 984 RPS durante 10 minutos
+        { duration: '5m', target: 3200 }, // Aumento del 5% a 1,033 RPS durante 10 minutos
+        { duration: '5m', target: 3400 }, // Aumento del 5% a 1,085 RPS durante 10 minutos
+        { duration: '5m', target: 3600 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
+        { duration: '5m', target: 3800 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
+        { duration: '5m', target: 4000 }, // Aumento del 5% a 1,139 RPS durante 10 minutos
     ],
     thresholds: {
         // Define un umbral para asegurar que el 95% de las peticiones tengan un tiempo de respuesta menor a 2 segundos
