3177512672dfe025fb3957c9d0a473363a6316ac
Alberto Gomez <alber82@gmail.com>
2025-03-17 16:16:58 +0100

    nginx tests

--- enviroment/install.sh
+++ enviroment/install.sh
@@ -7,6 +7,9 @@ influx bucket create --name doctorado --org uclm --retention 72h
 helm upgrade --install telegraf -n monitoring -f values-telegraf.yaml influxdata/telegraf
 helm install kube-prometheus-stack   --create-namespace   --namespace monitoring   prometheus-community/kube-prometheus-stack -f values-prometheus-stack.yaml
 
+helm uninstall kube-prometheus-stack    --namespace monitoring
+
+
 # instalacion k6 operator
 helm install k6-operator grafana/k6-operator -f values.yaml
 
