b9c98abde13c98b7f9a12e90c4d7e1dcefe942e7
Alberto Gomez <alber82@gmail.com>
2025-07-07 10:41:45 +0200

    sec

--- enviroment/create-secret.sh
+++ enviroment/create-secret.sh
@@ -1,15 +1,12 @@
 kubectl create secret generic prometheus-tls \
   --from-file=tls.crt=tls/prometheus.crt \
   --from-file=tls.key=tls/prometheus.key \
-  --from-file=ca.crt=tls/my-ca.crt \
-  --from-file=web-config.yml=<(cat <<EOF
-tls_server_config:
-  cert_file: /tls/tls.crt
-  key_file: /tls/tls.key
-EOF
-) \
   -n monitoring
 
 kubectl create secret generic telegraf-ca \
-  --from-file=ca.crt=tls/my-ca.crt \
+  --from-file=ca.crt=tls/ca.crt \
+  -n monitoring
+
+kubectl create configmap node-exporter-web-config \
+  --from-file=web-config.yml=web-config.yml \
   -n monitoring
